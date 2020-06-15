ace87eec2a941cf70951a1b784e0517317d6d0a3
heidihendry <10040864+heidihendry@users.noreply.github.com>
2020-06-15 07:56:12 +0700

    Delete passwordsLastSet (1).ps1

--- passwordsLastSet (1).ps1
+++
@@ -1,9 +0,0 @@
-import-module ActiveDirectory
-
-
-import-csv “c:\scripts\ExpiredPasswords\passwordsLastSet2.csv” | ForEach-Object {
-$User = '[ADSI]"LDAP://' + $_.DistinguishedName + '"'
-$User.pwdLastSet = 0
-$User.SetInfo()
-Write-Host $User
-}
