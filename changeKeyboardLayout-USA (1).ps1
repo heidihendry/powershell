916c915c2d003f61484e5fb3a202300071413942
heidihendry <10040864+heidihendry@users.noreply.github.com>
2020-06-15 07:55:11 +0700

    Delete changeKeyboardLayout-USA (1).ps1

--- changeKeyboardLayout-USA (1).ps1
+++
@@ -1,4 +0,0 @@
-$langList = New-WinUserLanguageList en-au
-$langList[0].InputMethodTips.Clear()
-$langList[0].InputMethodTips.Add('0C09:00000409') # English (Australia) - Keyboard USA
-Set-WinUserLanguageList $langList
